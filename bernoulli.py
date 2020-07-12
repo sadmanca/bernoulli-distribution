b8203757ed1f038209c7129c2b5fae555c46ba60
Sadman Hossain <m.sadman.h@gmail.com>
2020-07-12 10:21:09 -0400

    Add random variate generator

--- bernoulli.py
+++ bernoulli.py
@@ -1,3 +1,5 @@
+import numpy
+
 # probabilty mass function (pmf)
 # where x is a random variable and p is the probabilty
 # of an event with a Bernoulli distribution
@@ -15,3 +17,16 @@ def var(p):
 # standard deviation as root of variance
 def std(p):
     return var(p)**(1/2)
+
+# generate random variates corresponding to a Bernoulli distribution
+# size determines number of generated variates
+SIZE = 1
+def rvs(p,SIZE):
+    rvs = numpy.array([])
+    for i in range(0,SIZE):
+        if numpy.random.rand() <= p:
+            a = 1
+        else:
+            a = 0
+        rvs = numpy.append(rvs,a)
+    return rvs
