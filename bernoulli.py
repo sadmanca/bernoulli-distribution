5a49bab0c7898db8cce8638b31acbc667d6f68e7
Sadman Hossain <m.sadman.h@gmail.com>
2020-07-12 10:26:38 -0400

    Add headers

--- bernoulli.py
+++ bernoulli.py
@@ -1,32 +1,44 @@
 import numpy
 
-# probabilty mass function (pmf)
-# where x is a random variable and p is the probabilty
-# of an event with a Bernoulli distribution
-def pmf(x,p):
-    return (p**x)*((1-p)**(1-x))
+class bernoulli():
+    def pmf(x,p):
+        """
+        probabilty mass function (pmf)
 
-# mean (expected value E(x)) of a Bernoulli random variable x
-def mean(p):
-    return p
+        where x is a random variable and p is the probabilty
+        of an event with a Bernoulli distribution
+        """
+        return (p**x)*((1-p)**(1-x))
 
-# variance of random variable x
-def var(p):
-    return p*(1-p)
+    def mean(p):
+        """
+        mean (expected value E(x)) of a Bernoulli random variable x
+        """
+        return p
 
-# standard deviation as root of variance
-def std(p):
-    return var(p)**(1/2)
+    def var(p):
+        """
+        variance of bernoulli random variable x
+        """
+        return p*(1-p)
 
-# generate random variates corresponding to a Bernoulli distribution
-# size determines number of generated variates
-SIZE = 1
-def rvs(p,SIZE):
-    rvs = numpy.array([])
-    for i in range(0,SIZE):
-        if numpy.random.rand() <= p:
-            a = 1
-        else:
-            a = 0
-        rvs = numpy.append(rvs,a)
-    return rvs
+    def std(p,self):
+        """
+        standard deviation as the square root of variance
+        """
+        return self.var(p)**(1/2)
+
+    def rvs(p,size):
+        """
+        generate random variates corresponding to a Bernoulli distribution
+
+        size determines number of generated variates
+        """
+        rvs = numpy.array([])
+        for i in range(0,size):
+            if numpy.random.rand() <= p:
+                a = 1
+            else:
+                a = 0
+            rvs = numpy.append(rvs,a)
+        return rvs
